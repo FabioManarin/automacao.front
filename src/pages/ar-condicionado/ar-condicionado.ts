83eac654f2f45b2e4cff245e9a44d16fa98f7839
fabio <fabiosantosmanarin@gmail.com>
2019-09-01 18:32:31 -0300

    Ajustado funcionalidades da climatização.

--- src/pages/ar-condicionado/ar-condicionado.ts
+++ src/pages/ar-condicionado/ar-condicionado.ts
@@ -1,10 +1,7 @@
-import { AlertProvider } from './../../providers/alert/alert';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import { HistoricoProvider } from '../../providers/historico/historico';
-import { HistoricoModel } from '../../app/models/historicoModel';
-import { UsuarioModel } from '../../app/models/usuarioModel';
 
 
 @IonicPage()
@@ -13,98 +10,32 @@ import { UsuarioModel } from '../../app/models/usuarioModel';
   templateUrl: 'ar-condicionado.html',
 })
 export class ArCondicionadoPage {
-
-  historico: HistoricoModel = new HistoricoModel();
-  usuarioLogado: UsuarioModel = new UsuarioModel();
-  historicoList: Array<HistoricoModel> = new Array<HistoricoModel>();
-  isCheckSuite: boolean = false;
-  isCheckQuarto: boolean = false;
-  isCheckSala: boolean = false;
+  isCheckVentilador: boolean = false;
+  isCheckAutomatico: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private bluetoothSerial: BluetoothSerial,
-    private historicoSrv: HistoricoProvider) {
+    private bluetoothSerial: BluetoothSerial) {
   }
 
   home(): void {
     this.navCtrl.setRoot('HomePage');
   }
 
-  ionViewDidLoad() {
-    this.historicoSrv.setUserLogado();
-    this.getStatus();
-  }
-
-  getStatus(): void {
-    this.getStatusSuite();
-    this.getStatusQuarto();
-    this.getStatusSala();
-  }
-
-  async getStatusSuite(): Promise<void> {
-    try {
-      this.isCheckSuite = await this.historicoSrv.getStatus('Arcondicionado suíte');
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async getStatusQuarto(): Promise<void> {
-    try {
-      this.isCheckQuarto = await this.historicoSrv.getStatus('Arcondicionado quarto');
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async getStatusSala(): Promise<void> {
-    try {
-      this.isCheckSala = await this.historicoSrv.getStatus('Arcondicionado sala');
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  arSuite(event){
-    let nome = 'Arcondicionado suíte';
+  ventilador(event){
     if(event.checked) {
-      this.ligarAr();
-      this.historicoSrv.save(nome, true);
+      this.bluetoothSerial.write('V1');
     } else {
-      this.desligarAr();
-      this.historicoSrv.save(nome, false);
+      this.bluetoothSerial.write('V0');
     }
   }
 
-  arQuarto(event){
-    let nome = 'Arcondicionado quarto';
+  ventiladorAutomatico(event){
     if(event.checked) {
-      this.ligarAr();
-      this.historicoSrv.save(nome, true);
+      this.bluetoothSerial.write('VA1');
     } else {
-      this.desligarAr();
-      this.historicoSrv.save(nome, false);
+      this.bluetoothSerial.write('VA0');
     }
-  }
-
-  arSala(event){
-    let nome = 'Arcondicionado sala';
-    if(event.checked) {
-      this.ligarAr();
-      this.historicoSrv.save(nome, true);
-    } else {
-      this.desligarAr();
-      this.historicoSrv.save(nome, false);
-    }
-  }
-
-  ligarAr() {
-    this.bluetoothSerial.write('AL');
-  }
-
-  desligarAr() {
-    this.bluetoothSerial.write('AD');
   }
 }
